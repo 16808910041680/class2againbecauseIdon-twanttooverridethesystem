f297390a7aac3bac566172fea0c7763b56e603d3
16808910041680 <forforfirefire@gmail.com>
2025-03-09 10:56:35 +1100

    Print And Input

--- hi.py
+++ hi.py
@@ -3,3 +3,14 @@ print ("Because paragraphs are paragraphs on paragraphs,")
 print ("This paragraph is technically a paragraph of a paragraph")
 print ("Talking about a paragraph talking about a paragraph.")
 print ("And somehow this is still 1 paragraph.")
+x = "pneu"
+y = "mono"
+z = "ultra"
+a = "microscopic"
+b = "silico"
+c = "volcano"
+d = "coniosis"
+e = 47
+w = int (input("Random number please"))
+print (x, '\n',y,z,a,b,c,d,e)
+print (e,"+",w, "=", e+w) 
