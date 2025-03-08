8c52f9f5d68f49043771333473bcec95fcc97879
16808910041680 <forforfirefire@gmail.com>
2025-03-09 10:45:02 +1100

    i

---
+++ hi.py
@@ -0,0 +1,5 @@
+print ("I am writing a paragraph about writing a paragrph")
+print ("Because paragraphs are paragraphs on paragraphs,")
+print ("This paragraph is technically a paragraph of a paragraph")
+print ("Talking about a paragraph talking about a paragraph.")
+print ("And somehow this is still 1 paragraph.")
